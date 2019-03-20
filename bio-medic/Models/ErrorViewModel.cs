fac99794f85d542ec0e37d743879e16536f12966
Kinga T <kinga-tanska@wp.pl>
2019-03-20 16:56:22 +0100

    projekt:bio-medic

---
+++ bio-medic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bio_medic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
